32752686e8bd5817a0c2b11a165139b97ba0b965
Thomas Tilahun <thomm.sef@gmail.com>
2021-01-24 22:53:58 -0800

    I am able to fetch data from server using redux for all products and single product

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { ListProductDetails } from '../actions/productActions'
 
 
 const ProductScreen = ({ match }) => {
 
-    const [product, setProduct] = useState({})
+    const dispatch = useDispatch()
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, product, error } = productDetails
+  
+      useEffect(() => {
+          dispatch(ListProductDetails(match.params.id))
+          
+      }, [dispatch, match])
 
-        fetchProduct()
-    }, [match])
-
-    return (
+  return (
 
 <>
         <Link className='btn btn-light my-3' to='/'>
          Go Back
         </Link>
+        {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+
     <Row>
         <Col md={6}>
             <Image src={product.image} alt={product.name} fluid />
@@ -78,6 +82,7 @@ const ProductScreen = ({ match }) => {
             </Card>
         </Col>
     </Row>
+        )}
 
 </>
 
